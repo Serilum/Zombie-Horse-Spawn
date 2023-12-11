32ed4c0bce97e2bcbca4626fafc39bac7acd65fc
Rick South <rick@natamus.com>
2023-12-11 23:40:46 +0100

    Zombie Horse Spawn source code for 1.19.2.

---
+++ Forge/src/main/java/com/natamus/zombiehorsespawn/forge/events/ForgeZombieHorseEvent.java
@@ -0,0 +1,30 @@
+package com.natamus.zombiehorsespawn.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.zombiehorsespawn.events.ZombieHorseEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent.LevelTickEvent;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeZombieHorseEvent {
+	@SubscribeEvent
+	public void onEntityJoin(EntityJoinLevelEvent e) {
+		ZombieHorseEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+	
+	@SubscribeEvent
+	public void onWorldTick(LevelTickEvent e) {
+		Level level = e.level;
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		ZombieHorseEvent.onWorldTick((ServerLevel)level);
+	}
+}
