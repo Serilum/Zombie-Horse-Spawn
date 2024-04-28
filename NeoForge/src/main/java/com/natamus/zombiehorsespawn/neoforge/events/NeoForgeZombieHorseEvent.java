291ffef12dee6d50e9441325084c1ccf0dd796c9
Rick South <rick@natamus.com>
2024-04-28 18:55:27 +0200

    (v5.0) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/zombiehorsespawn/neoforge/events/NeoForgeZombieHorseEvent.java
+++ NeoForge/src/main/java/com/natamus/zombiehorsespawn/neoforge/events/NeoForgeZombieHorseEvent.java
@@ -3,8 +3,7 @@ package com.natamus.zombiehorsespawn.neoforge.events;
 import com.natamus.zombiehorsespawn.events.ZombieHorseEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.LevelTickEvent;
-import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -17,9 +16,9 @@ public class NeoForgeZombieHorseEvent {
 	}
 	
 	@SubscribeEvent
-	public static void onWorldTick(LevelTickEvent e) {
-		Level level = e.level;
-		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+	public static void onWorldTick(LevelTickEvent.Pre e) {
+		Level level = e.getLevel();
+		if (level.isClientSide) {
 			return;
 		}
 
