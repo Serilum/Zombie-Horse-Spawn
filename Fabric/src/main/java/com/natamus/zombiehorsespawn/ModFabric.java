6aa0137778b940fa113c8b92e8f466a1429775e7
Rick South <rick@natamus.com>
2024-05-01 16:14:35 +0200

    Zombie Horse Spawn source code for 1.20.6.

---
+++ Fabric/src/main/java/com/natamus/zombiehorsespawn/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.zombiehorsespawn;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.zombiehorsespawn.events.ZombieHorseEvent;
+import com.natamus.zombiehorsespawn.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		ServerTickEvents.START_WORLD_TICK.register((ServerLevel world) -> {
+			ZombieHorseEvent.onWorldTick(world);
+		});
+
+		ServerEntityEvents.ENTITY_LOAD.register((Entity entity, ServerLevel world) -> {
+			ZombieHorseEvent.onEntityJoin(world, entity);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
