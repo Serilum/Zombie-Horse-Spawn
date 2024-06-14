5a93f6253189962540337387ea0b2546591566f7
Rick South <rick@natamus.com>
2024-06-14 05:04:22 +0200

    Zombie Horse Spawn source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/zombiehorsespawn/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.zombiehorsespawn.util;
+
+public class Reference {
+	public static final String MOD_ID = "zombiehorsespawn";
+	public static final String NAME = "Zombie Horse Spawn";
+	public static final String VERSION = "5.0";
+	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
+}
