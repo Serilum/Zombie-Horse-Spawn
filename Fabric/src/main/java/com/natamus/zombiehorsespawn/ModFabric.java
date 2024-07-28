72308679b6a108137e677f1f45bc0b0b68c65e2d
Rick South <rick@natamus.com>
2024-07-28 02:34:25 +0200

    (v5.1) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/zombiehorsespawn/ModFabric.java
+++ Fabric/src/main/java/com/natamus/zombiehorsespawn/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.zombiehorsespawn;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.zombiehorsespawn.events.ZombieHorseEvent;
 import com.natamus.zombiehorsespawn.util.Reference;
 import net.fabricmc.api.ModInitializer;
@@ -13,6 +14,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- NeoForge/src/main/java/com/natamus/zombiehorsespawn/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/zombiehorsespawn/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.zombiehorsespawn;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.zombiehorsespawn.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.zombiehorsespawn.neoforge.events.NeoForgeZombieHorseEvent;
 import com.natamus.zombiehorsespawn.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
