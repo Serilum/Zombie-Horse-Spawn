d54589141946ce1433652eaf3f8671c1db24d8e5
Rick South <rick@natamus.com>
2025-02-12 23:06:25 +0100

    (v5.2) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/zombiehorsespawn/ModForge.java
+++ Forge/src/main/java/com/natamus/zombiehorsespawn/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-		MinecraftForge.EVENT_BUS.register(new ForgeZombieHorseEvent());
+		MinecraftForge.EVENT_BUS.register(ForgeZombieHorseEvent.class);
 	}
 
 	private static void setGlobalConstants() {
