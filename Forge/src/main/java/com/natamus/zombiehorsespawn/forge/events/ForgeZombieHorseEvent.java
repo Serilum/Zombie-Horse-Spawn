a4aa6bff0fb00792b2a5d61af93264bd6ae74103
Rick South <rick@natamus.com>
2025-02-12 23:06:11 +0100

    (v5.2) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/zombiehorsespawn/forge/events/ForgeZombieHorseEvent.java
+++ Forge/src/main/java/com/natamus/zombiehorsespawn/forge/events/ForgeZombieHorseEvent.java
@@ -9,15 +9,14 @@ import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeZombieHorseEvent {
 	@SubscribeEvent
-	public void onEntityJoin(EntityJoinLevelEvent e) {
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
 		ZombieHorseEvent.onEntityJoin(e.getLevel(), e.getEntity());
 	}
 	
 	@SubscribeEvent
-	public void onWorldTick(LevelTickEvent e) {
+	public static void onWorldTick(LevelTickEvent e) {
 		Level level = e.level;
 		if (level.isClientSide || !e.phase.equals(Phase.START)) {
 			return;
